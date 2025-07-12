895e944969dd5a02f0a68d48192be0da45239034
Sistemas picolargo <mttosistemas@picolargo.com>
2025-07-12 10:41:47 -0600

    Se agregó la skin color naranga para el formulario de material skin2

--- Tablero/Form1.cs
+++ Tablero/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +12,16 @@ using System.Windows.Forms;
 
 namespace Tablero
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         public Form1()
         {
             InitializeComponent();
+            // Initialize MaterialSkinManager and set the theme and color scheme
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Orange600, Primary.Orange600, Primary.BlueGrey800, Accent.Orange700, TextShade.WHITE);
         }
     }
 }
